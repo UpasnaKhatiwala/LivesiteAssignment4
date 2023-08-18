753e8b595b00ef58a755d2cd7fa87958b35e7870
Upasna Khatiwala <200573736@student.georgianc.on.ca>
2023-08-18 13:30:46 -0400

    added livesite file

---
+++ Server/Config/db.ts
@@ -0,0 +1,9 @@
+let localURI= 'mongodb://127.0.0.1:27017/movies';
+let remoteURI='mongodb+srv://upasnakhatiwala3:assignment4@movies.upokyeg.mongodb.net/Movies';
+let secret = 'SomeSecret';
+
+export default {
+    localURI: localURI,
+    remoteURI: remoteURI,
+    secret: secret
+}
